ac808d145290c2321500903ba01d0a54d05ab8c7
67millwood <info@track2development.com>
2018-10-25 18:29:50 +0000

    revised

--- mapexercise.js
+++ mapexercise.js
@@ -4,6 +4,14 @@ map(words, function(word) {
   return word.length;
 });
 
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
 function map(input, callback) {
   var newArray =[]
   for (word in input) {
