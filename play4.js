0bdfdda82a840676a2376201c02fdcebd5bb4323
67millwood <info@track2development.com>
2018-10-25 17:22:40 +0000

    just starting

---
+++ play4.js
@@ -0,0 +1,22 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+function super(words, fun1, fun2, fun3) {
+  words.forEach((item) => {
+
+
+  })
+
+}
+
